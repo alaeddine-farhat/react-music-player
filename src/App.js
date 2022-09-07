7165708f873051bfdd8a6a9bb5102f318723fe6d
Alaeddine Farhat <alaedinne.farhat@gmail.com>
2022-09-07 09:05:50 +0200

    init to main

---
+++ src/App.js
@@ -0,0 +1,123 @@
+import React, { useState, useRef } from "react";
+import styled from "styled-components";
+import "./App.css";
+
+// Import components
+import Player from "./components/Player";
+import Song from "./components/Song";
+import Library from "./components/Library";
+import Nav from "./components/Nav";
+import Credit from "./components/Credit";
+// Import data
+import data from "./data";
+
+const App = () => {
+	// Ref
+	const audioRef = useRef(null);
+
+	// State
+	const [songs, setSongs] = useState(data());
+	const [currentSong, setCurrentSong] = useState(songs[0]);
+	const [isPlaying, setIsPlaying] = useState(false);
+	const [libraryStatus, setLibraryStatus] = useState(false);
+	const [songInfo, setSongInfo] = useState({
+		currentTime: 0,
+		duration: 0,
+	});
+
+	// Functions
+	const updateTimeHandler = (e) => {
+		const currentTime = e.target.currentTime;
+		const duration = e.target.duration;
+		setSongInfo({ ...songInfo, currentTime, duration });
+	};
+
+	const songEndHandler = async () => {
+		let currentIndex = songs.findIndex((song) => song.id === currentSong.id);
+		let nextSong = songs[(currentIndex + 1) % songs.length];
+		setCurrentSong(nextSong);
+
+		const newSongs = songs.map((song) => {
+			if (song.id === nextSong.id) {
+				return {
+					...song,
+					active: true,
+				};
+			} else {
+				return {
+					...song,
+					active: false,
+				};
+			}
+		});
+		setSongs(newSongs);
+
+		if (isPlaying) {
+			audioRef.current.play();
+		}
+	};
+
+	return (
+		<AppContainer libraryStatus={libraryStatus}>
+			<Nav libraryStatus={libraryStatus} setLibraryStatus={setLibraryStatus} />
+			<Song currentSong={currentSong} />
+			<Player
+				isPlaying={isPlaying}
+				setIsPlaying={setIsPlaying}
+				currentSong={currentSong}
+				setCurrentSong={setCurrentSong}
+				audioRef={audioRef}
+				songInfo={songInfo}
+				setSongInfo={setSongInfo}
+				songs={songs}
+				setSongs={setSongs}
+			/>
+			<Library
+				songs={songs}
+				setCurrentSong={setCurrentSong}
+				audioRef={audioRef}
+				isPlaying={isPlaying}
+				setSongs={setSongs}
+				libraryStatus={libraryStatus}
+			/>
+			{/* <Credit /> */}
+			<audio
+				onLoadedMetadata={updateTimeHandler}
+				onTimeUpdate={updateTimeHandler}
+				onEnded={songEndHandler}
+				ref={audioRef}
+				src={currentSong.audio}
+			/>
+			<PreContainer>
+				<Pre>{currentSong.lyrics}</Pre>
+			</PreContainer>
+		</AppContainer>
+	);
+};
+
+const AppContainer = styled.div`
+	transition: all 0.5s ease;
+	margin-left: ${(p) => (p.libraryStatus ? "20rem" : "0")};
+	@media screen and (max-width: 768px) {
+		margin-left: 0;
+	}
+`;
+const PreContainer = styled.div`
+display: flex;
+justify-content: center;
+`
+const Pre = styled.pre`
+overflow-x:hidden;
+overflow-y: auto;
+white-space:pre-wrap;
+word-wrap: break-word;
+font-size: 30px;
+text-align: center;
+width: 80vw;
+max-height: 45vh;
+@media screen and (max-width: 768px) {
+		width: 100vw;
+	}
+`
+
+export default App;
